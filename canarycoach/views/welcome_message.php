8ef3c03c15d1fd8918678c71f55945e837c0a141
luister <el_terminator93@hotmail.com>
2015-03-26 19:19:54 +0000

    slide para las actividades
    slide para las actividades

--- canarycoach/views/welcome_message.php
+++ canarycoach/views/welcome_message.php
@@ -1,5 +1,150 @@
+<!-- En este div colocaremos un slide de fotos inicial -->
 <div id="owl-demo" class="owl-carousel owl-theme">
     <div class="item"><img class="img-responsive" src="<?php echo $resources.'/images/image1.jpg'?>" ></div>
     <div class="item"><img class="img-responsive" src="<?php echo $resources.'/images/image2.jpg'?>" ></div>
     <div class="item"><img class="img-responsive" src="<?php echo $resources.'/images/image3.jpg'?>" ></div>
+</div>
+
+<!-- En este div colocaremos un texto de bienvenida a la pagina -->
+<div class="container">
+	<div class="jumbotron" id="textoBienvenida">
+	  <h3>Bienvenidos a la comunidad canaria de intercambio de actividades</h3>
+	  <p>
+	  	<pre>
+	      	Este es tu sitio web ideal para publicitar la actividad
+	      	que desees impartir o incluso unirte a aquella que
+	      	más te guste. No lo dudes y únete, la experiencia será
+	      	inolvidable.
+	      </pre>
+	  </p>
+	  <!-- <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p> -->
+	</div>
+</div>
+
+<!-- En este div colocaremos las actividades recientes -->
+<div class="container">
+	<div class="jumbotron" id="textoBienvenida">
+	  	<h3>Actividades recientes</h3>
+	  	<!-- <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p> -->
+	  	<div class="row">
+		  <div class="col-sm-6 col-md-3">
+		    <div class="thumbnail">
+		      <img src="..." alt="...">
+		      <div class="caption">
+		        <h3>Thumbnail label</h3>
+		        <p>Esta el la primera actividad</p>
+		        <!-- <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p> -->
+		        <p><a class="btn btn-primary btn-lg" href="#" role="button">Ver +</a></p>
+		      </div>
+		    </div>
+		  </div>
+
+		  <div class="col-sm-6 col-md-3">
+		    <div class="thumbnail">
+		      <img src="..." alt="...">
+		      <div class="caption">
+		        <h3>Thumbnail label</h3>
+		        <p>Esta el la segunda actividad</p>
+		        <!-- <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p> -->
+		        <p><a class="btn btn-primary btn-lg" href="#" role="button">Ver +</a></p>
+		      </div>
+		    </div>
+		  </div>
+
+		  <div class="col-sm-6 col-md-3">
+		    <div class="thumbnail">
+		      <img src="..." alt="...">
+		      <div class="caption">
+		        <h3>Thumbnail label</h3>
+		        <p>Esta el la tercera actividad</p>
+		        <!-- <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p> -->
+		        <p><a class="btn btn-primary btn-lg" href="#" role="button">Ver +</a></p>
+		      </div>
+		    </div>
+		  </div>
+
+		  <div class="col-sm-6 col-md-3">
+		    <div class="thumbnail">
+		      <img src="..." alt="...">
+		      <div class="caption">
+		        <h3>Thumbnail label</h3>
+		        <p>Esta el la cuarta actividad</p>
+		        <!-- <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p> -->
+		        <p><a class="btn btn-primary btn-lg" href="#" role="button">Ver +</a></p>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+
+		<nav>
+		  <ul class="pager">
+		    <li><a href="#">Anterior</a></li>
+		    <li><a href="#">Siguiente</a></li>
+		  </ul>
+		</nav>
+	</div>
+</div>
+
+<!-- En este div colocaremos las actividades mas destacadas -->
+<div class="container">
+	<div class="jumbotron" id="textoBienvenida">
+		<h3>Actividades más destacadas</h3>
+	  	<!-- <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p> -->
+	  	<div class="row">
+		  <div class="col-sm-6 col-md-3">
+		    <div class="thumbnail">
+		      <img src="..." alt="...">
+		      <div class="caption">
+		        <h3>Thumbnail label</h3>
+		        <p>Esta el la primera actividad</p>
+		        <!-- <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p> -->
+		        <p><a class="btn btn-primary btn-lg" href="#" role="button">Ver +</a></p>
+		      </div>
+		    </div>
+		  </div>
+
+		  <div class="col-sm-6 col-md-3">
+		    <div class="thumbnail">
+		      <img src="..." alt="...">
+		      <div class="caption">
+		        <h3>Thumbnail label</h3>
+		        <p>Esta el la segunda actividad</p>
+		        <!-- <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p> -->
+		        <p><a class="btn btn-primary btn-lg" href="#" role="button">Ver +</a></p>
+		      </div>
+		    </div>
+		  </div>
+
+		  <div class="col-sm-6 col-md-3">
+		    <div class="thumbnail">
+		      <img src="..." alt="...">
+		      <div class="caption">
+		        <h3>Thumbnail label</h3>
+		        <p>Esta el la tercera actividad</p>
+		        <!-- <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p> -->
+		        <p><a class="btn btn-primary btn-lg" href="#" role="button">Ver +</a></p>
+		      </div>
+		    </div>
+		  </div>
+
+		  <div class="col-sm-6 col-md-3">
+		    <div class="thumbnail">
+		      <img src="..." alt="...">
+		      <div class="caption">
+		        <h3>Thumbnail label</h3>
+		        <p>Esta el la cuarta actividad</p>
+		        <!-- <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p> -->
+		        <p><a class="btn btn-primary btn-lg" href="#" role="button">Ver +</a></p>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+
+		<nav>
+		  <ul class="pager">
+		    <li><a href="#">Anterior</a></li>
+		    <li><a href="#">Siguiente</a></li>
+		  </ul>
+		</nav>
+	</div>
 </div>
