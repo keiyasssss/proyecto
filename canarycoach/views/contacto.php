173ef93d2963050e8fba4f35a9a8cc787cb9ef02
luister <el_terminator93@hotmail.com>
2015-03-27 16:29:24 +0000

    actualizacion 2
    actualizacion 2

--- canarycoach/views/contacto.php
+++ canarycoach/views/contacto.php
@@ -11,7 +11,7 @@
             <input type="email" class="form-control" id="InputEmailCon" placeholder="Introduzca su correo">
         </div>
         <div class="form-group">
-           <label for="TextArea">Introduzca su texto:</label>
+            <label for="TextArea">Introduzca su texto:</label>
             <textarea class="form-control" rows="3" id="TextAreaCon"></textarea>
         </div>
         
--- canarycoach/views/templates/menu.php
+++ canarycoach/views/templates/menu.php
@@ -3,7 +3,8 @@
     <li> <a href="http://localhost/proyecto"><img class="img-responsive" src="<?php echo $resources.'/img/canarycoach.png'?>" width="200" height="90" title="canarycoach" alt="canarycoach"></a></li>
     <li role="presentation"><button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#registrarseModal">Registrarse</button></li>
     <li role="presentation"><button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#iniciarSesionModal">Iniciar sesión</button></li>
-    <li role="presentation"><button class="btn btn-primary btn-lg" onclick=window.open("<?php echo $base_url; ?>/Contacto","_self")>Contacto</button></li>
+    <!-- <li role="presentation"><button class="btn btn-primary btn-lg" onclick=window.open("<?php echo $base_url; ?>/Contacto","_self")>Contacto</button></li> -->
+    <li role="presentation"><button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#contactoModal">Contacto</button></li>    
     <li role="presentation"><button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#ayudaModal">Ayuda</button></li>
   </ul>
 </div>
@@ -75,6 +76,47 @@
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">cerrar</button>
         <button type="button" class="btn btn-primary">Iniciar Sesión</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<!-- Modal contacto -->
+<div class="modal fade" id="contactoModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Contacto</h4>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="InputNombreReg">Nombre:</label>
+            <input type="text" class="form-control" id="InputNombreReg" placeholder="Introduzca su nombre">
+          </div>
+          <div class="form-group">
+            <label for="InputEmailReg">Correo electrónico:</label>
+            <input type="email" class="form-control" id="InputEmailReg" placeholder="Introduzca su correo">
+          </div>
+          <div class="form-group">
+            <label for="InputContraseñaReg">Contraseña:</label>
+            <input type="password" class="form-control" id="InputContraseñaReg" placeholder="Introduzca su contraseña">
+          </div>
+          <div class="form-group">
+            <label for="InputContraseñaReg2">Repita su contraseña:</label>
+            <input type="password" class="form-control" id="InputContraseñaReg2" placeholder="Repita su contraseña">
+          </div>
+          <div class="form-group">
+            <label for="TextArea">Introduzca su texto:</label>
+            <textarea class="form-control" rows="3" id="TextAreaCon"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">cerrar</button>
+        <!-- <button type="button" class="btn btn-primary">Registrarse</button> -->
+        <button type="button" class="btn btn-primary">Enviar</button>
       </div>
     </div><!-- /.modal-content -->
   </div><!-- /.modal-dialog -->
