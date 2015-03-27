035ea296823e3d6d9f352c0d845d9ef89aea1f99
luister <el_terminator93@hotmail.com>
2015-03-27 16:08:18 +0000

    cambios footer
    cambios footer

--- canarycoach/views/templates/footer.php
+++ canarycoach/views/templates/footer.php
@@ -1,10 +1,11 @@
   <div class="container">
-    <ul class="list-inline">
-      <li> <a href="http://localhost/proyecto"><img class="img-responsive" src="<?php echo $resources.'/img/canarycoach.png'?>" width="200" height="90" title="canarycoach" alt="canarycoach"></a></li>
-      <li>FB</li>
-      <li>G+</li>
+    <ul class="list-inline" align="center">
+      <li> <a href="http://localhost/proyecto"><img class="img-responsive" src="<?php echo $resources.'/img/canarycoach.png'?>" width="300" height="200" title="Canary Coach" alt="canarycoach"></a></li>
+      <li><a href="http://www.facebook.com"><img class="img-responsive" src="<?php echo $resources.'/img/Facebook.png'?>" title="Facebook" alt="facebook"></a></li>
+      <li><a href="http://plus.google.com"><img class="img-responsive" src="<?php echo $resources.'/img/google_plus.png'?>" title="Google Plus" alt="googleplus"></a></li>
     </ul>
   </div>
+
   <script> 
       $(document).ready(function() {
         $("#owl-principal").owlCarousel({
@@ -25,5 +26,4 @@
       
     </script>
   </body>
-
 </html>
